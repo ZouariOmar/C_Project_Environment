88aaf25f86f46b063293b5070a136b1d86e833fb
Zouari Omar <zouariomar20@gmail.com>
2024-01-06 16:31:52 +0100

    first commit

---
+++ Library/lib.c
@@ -0,0 +1,9 @@
+// by_zouari_omar_full_stack_c++_dev
+// dd/mm/yyyy
+// Name Of Project Or Name The File
+                    /* INCLUDE PROTOTYPE DECLARATION PART */
+#include "../include/inc.h"
+                    /* FUNCTIONS PROTOTYPE DEV PART */
+/*
+func...
+*/
---
+++ Source/main.c
@@ -0,0 +1,11 @@
+// by_zouari_omar_full_stack_c/c++_dev
+// dd/mm/yyyy
+// Name Of Project Or Name The File
+                    /* INCLUDE PROTOTYPE DECLARATION PART */
+#include "../Library/lib.c"
+                    /* MAIN() INT FUNCTION DEV PART */
+int main(int argc, char const *argv[]) {
+    // principal vars declaration part
+    printf("Hello World\n");
+    return 0;
+}
